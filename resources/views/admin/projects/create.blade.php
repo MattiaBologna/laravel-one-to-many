5e7b717948de4238064b77b9e045aac085aa8c26
MattiaBologna <mattiabologna01@gmail.com>
2024-06-06 17:18:58 +0200

    Add: type select on project create

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -17,6 +17,15 @@
             <input type="text" class="form-control" name='description' placeholder="Description">
         </div>
         <div>
+            <label for="type_id" class="form-label">Select type</label>
+            <select name="type_id" id="type_id" class="form-control">
+                <option value="">-- Select Type --</option>
+                @foreach ($types as $type)
+                    <option @selected($type->id == old('type_id')) value="{{$type->id}}">{{$type->name}}</option>
+                @endforeach
+            </select>
+        </div>
+        <div>
             <label class="form-label fs-5" for="link">Projet Github Link</label>
             <input type="text" class="form-control" name='link' placeholder="Github Link">
         </div>
